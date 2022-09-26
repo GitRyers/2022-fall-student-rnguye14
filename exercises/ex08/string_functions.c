f88774eb1acc9c0c39eb693ea349abcfd86c3d5a
Ryan Nguyen <rnguye14@jh.edu>
2022-09-26 18:12:03 -0400

    finalized ex08

---
+++ exercises/ex08/string_functions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "string_functions.h"
+
+int concat(const char word1[], const char word2[], char result[], int result_capacity){
+
+   //TODO: replace the following stub with a correct function body so that
+   //      this function behaves as indicated in the comment above
+  int length = strlen(word1) + strlen(word2) + 1;
+
+  printf("This is the length: %d\n", length); 
+  
+  if (length > result_capacity) {
+    return 1;
+  }
+  else {
+    int i, j;
+
+    for (i = 0; word1[i] != '\0'; ++i) {
+      result[i] = (char) word1[i];
+    }
+
+    for (j = 0; word2[j] != '\0'; ++i, ++j) {
+      result[i] = (char) word2[j];
+    }
+    result[i] = '\0';
+
+    return 0;
+  }
+}
