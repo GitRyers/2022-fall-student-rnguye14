0f42f38dca7783910b747cd5a44a52f438769550
Ryan Nguyen <rnguye14@jh.edu>
2022-09-29 22:57:06 -0400

    finalized

--- homework/hw3/search_functions.c
+++ homework/hw3/search_functions.c
@@ -5,6 +5,9 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include "search_functions.h"
 
 
@@ -16,59 +19,188 @@
  * If the file contains an invalid grid, this function returns -2.
  */
 int populate_grid(char grid[][MAX_SIZE], char filename_to_read_from[]){
+  FILE* file = fopen(filename_to_read_from, "r");
+  int n = 0;
+  char ch1; 
+  while ((ch1 = fgetc(file)) != '\n') {
+    if (isalpha(ch1) != 0) {
+      n++;
+    }
+  }
+  
+  //Creates grid
+  int i = 0;
+  int j = 0; 
+  char ch2;
+  rewind(file); 
+  while ((ch2 = fgetc(file)) != EOF) {
+    if (i == n) {
+      break;
+    }
+    if (ch2 == '\n') {
+      i++;
+      j = 0; 
+      continue; 
+    }
+    else { 
+      grid[i][j] = tolower(ch2);
+      j++; 
+    }
+  }
+  fclose(file); 
+  return n;  
+}
 
-  return -1; // replace this stub
+void strlower(char *str) {
+  int len = strlen(str);   
+  for(int i = 0; i < len; i++){
+    str[i] = tolower(str[i]);
+  }  
+}
 
+void strrev(char *str){
+  char *p1, *p2;
+  for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2){
+    *p1 ^= *p2;
+    *p2 ^= *p1;
+    *p1 ^= *p2;
+  }
+}
+
+int find_right(char grid[][MAX_SIZE], int n, char word[], FILE *write_to){ 
+  //Initializing search word  
+  int num_found = 0;
+  int len = strlen(word);
+  strlower(word); 
+  
+
+  //Finding words  
+  for (int i = 0; i < n; i++) {
+    int pos = 0;      
+    while ((pos <= (n-len))) {
+      char *cmp_word = malloc(sizeof(char) * (len + 1));   
+      for (int j = 0; j < len; j++) {
+	      *(cmp_word + j) = grid[i][j + pos];
+      } 
+      cmp_word[len] = '\0'; 
+      if (strcmp(word, cmp_word) == 0) { 
+	      fprintf(write_to, "%s %d %d R\n", word, i, pos);
+        num_found++;  
+      }
+      free(cmp_word); 
+      pos++; 
+     }
+    }
+  if (num_found == 0) {
+    printf("%s - Not found\n", word); 
+  }
+  return 0; 
 }
 
 
-/* 
- * <Replace this with your own useful comment.> 
- */
-int find_right(char grid[][MAX_SIZE], int n, char word[], FILE *write_to){
-
-  return -1; // replace this stub
-
-}
-
-
-/* 
- * <Replace this with your own useful comment.> 
- */
 int find_left (char grid[][MAX_SIZE], int n, char word[], FILE *write_to){
-
-  return -1; // replace this stub
-
+  //Initializing search word
+  int num_found = 0;
+  int len = strlen(word);
+  strlower(word);
+  strrev(word);  
+  
+  //Finding words          
+  for (int i = 0; i < n; i++) {
+    int pos = 0;     
+    while ((pos <= (n-len))) {
+      char *cmp_word = malloc(sizeof(char) * (len + 1));   
+      for (int j = 0; j < len; j++) {
+	      *(cmp_word + j) = grid[i][j + pos];
+      } 
+      cmp_word[len] = '\0'; 
+      if (strcmp(word, cmp_word) == 0) {
+        strrev(word);
+	      fprintf(write_to, "%s %d %d L\n", word, i, pos);
+        num_found++;  
+        strrev(word); 
+      }
+      free(cmp_word); 
+      pos++; 
+     }
+    } 
+  strrev(word);  
+  if (num_found == 0) {
+    printf("%s - Not found\n", word); 
+  }
+  return 0; 
 }
 
 
-/* 
- * <Replace this with your own useful comment.> 
- */
 int find_down (char grid[][MAX_SIZE], int n, char word[], FILE *write_to){
+  //Initializing search word  
+  int num_found = 0;
+  int len = strlen(word);
+  strlower(word); 
 
-  return -1; // replace this stub
-
+  //Finding words 
+  for (int i = 0; i < n; i++) {
+    int pos = 0;     
+    while ((pos <= (n-len))) {
+      char *cmp_word = malloc(sizeof(char) * (len + 1));   
+      for (int j = 0; j < len; j++) {
+	      *(cmp_word + j) = grid[j + pos][i];
+      } 
+      cmp_word[len] = '\0'; 
+      if (strcmp(word, cmp_word) == 0) { 
+	      fprintf(write_to, "%s %d %d D\n", word, pos, i);
+        num_found++;  
+      }
+      free(cmp_word); 
+      pos++; 
+     }
+    }
+  if (num_found == 0) {
+    printf("%s - Not found\n", word); 
+  }
+  return 0; 
 }
 
 
-/* 
- * <Replace this with your own useful comment.> 
- */
 int find_up   (char grid[][MAX_SIZE], int n, char word[], FILE *write_to){
+  //Initializing search word  
+  int num_found = 0;
+  int len = strlen(word);
+  strlower(word);
+  strrev(word);  
 
-  return -1; // replace this stub
-
+  //Finding words 
+  for (int i = 0; i < n; i++) {
+    int pos = 0;     
+    while ((pos <= (n-len))) {
+      char *cmp_word = malloc(sizeof(char) * (len + 1));   
+      for (int j = 0; j < len; j++) {
+	      *(cmp_word + j) = grid[j + pos][i];
+      } 
+      cmp_word[len] = '\0'; 
+      if (strcmp(word, cmp_word) == 0) { 
+        strrev(word); 
+	      fprintf(write_to, "%s %d %d U\n", word, (pos + len - 1), i);
+        num_found++;
+        strrev(word);  
+      }
+      free(cmp_word); 
+      pos++; 
+     }
+    }
+  strrev(word);  
+  if (num_found == 0) {
+    printf("%s - Not found\n", word); 
+  }
+  return 0; 
 }
 
-
-/* 
- * <Replace this with your own useful comment.> 
- */
 int find_all  (char grid[][MAX_SIZE], int n, char word[], FILE *write_to) {
-
-  return -1; // replace this stub
-
+  find_right(grid, n, word, write_to);
+  find_left(grid, n, word, write_to); 
+  find_down(grid, n, word, write_to); 
+  find_up(grid, n, word, write_to); 
+  return 0; 
 } 
 
 
@@ -100,4 +232,3 @@ int file_eq(char lhs_name[], char rhs_name[]) {
   fclose(rhs);
   return match;
 }
-
